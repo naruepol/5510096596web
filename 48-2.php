5211a4ce6468cd9ba9039de09fab096c8ecc4fc4
5510096596 <naruapon@webmail.npru.ac.th>
2017-09-16 11:51:09 +0700

    add 48-2.php
    constant

---
+++ 48-2.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+
+define(MAJOR,"Business Computer");
+define(YEAR,2547);
+
+print MAJOR."<br>";
+print YEAR;
+
+?>
+</body>
+</html>
