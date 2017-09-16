d66b0ed7ccd2a9292e4b406e74253ca2aade88dd
5510096596 <naruapon@webmail.npru.ac.th>
2017-09-16 11:55:59 +0700

    add 49.php
    system value

---
+++ 49.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+
+print PHP_VERSION;
+print "<br>";
+print PHP_OS;
+
+?>
+</body>
+</html>
