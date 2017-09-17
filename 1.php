ea81e0864e1635f230ef5597aa64e8a9235b46fd
naruapon <naruapon@webmail.npru.ac.th>
2017-09-17 10:52:47 +0700

    Add 1.php
    example

---
+++ 1.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>5510096596</title>
+</head>
+
+<body>
+<?php
+   print "Business Computer"."<br>";
+   $number = 1+2;
+   print $number;
+?>
+</body>
+</html>
