a3f0e82f5cd206c0e5af3367e11353dfbd987173
5510096596 <naruapon@webmail.npru.ac.th>
2017-09-23 10:52:36 +0700

    up form

---
+++ form.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form action="/action_page.php">
+  First name:<br>
+  <input type="text" value="Mickey"><br>
+  Last name:<br>
+  <input type="text" name="lastname" value="Mouse"><br><br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
